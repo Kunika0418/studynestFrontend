44a2ecf160145824eb1f8e5b41ea8f43ae6753c7
Mayank Uchariya <mayankuchariya0@gmail.com>
2024-11-22 22:39:45 +0530

    initial

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,17 +1,17 @@
 import React from 'react';
 
-const ApartmentCard = ({ imageUrl, title, price, description, location }) => {
+const ApartmentCard = ({ image, title, price, description, city, country }) => {
   return (
     <div className="max-w-sm rounded-lg overflow-hidden shadow-lg bg-white">
       {/* Apartment Image */}
-      <img src={imageUrl} alt="Apartment" className="w-full h-64 object-cover" />
+      <img src={image} alt="Apartment" className="w-full h-64 object-cover" />
 
       {/* Apartment Details */}
       <div className="p-6">
         <h2 className="text-xl font-semibold text-gray-800 mb-2">{title}</h2>
-        <p className="text-lg font-semibold text-amber-800 mb-4">{price}</p>
+        <p className="text-lg font-semibold text-amber-800 mb-4">${price}/month</p>
         <p className="text-gray-600 text-base mb-4">{description}</p>
-        <p className="text-sm text-gray-500">Location: {location}</p>
+        <p className="text-sm text-gray-500">Location: {city}/{country}</p>
       </div>
 
       {/* Button to view details */}
