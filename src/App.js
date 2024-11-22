f2673aa84af489c8df41f7a2fa1eea8892aa9048
Mayank Uchariya <mayankuchariya0@gmail.com>
2024-11-22 16:19:22 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import Navabar from "./components/Navbar/Navbar.js";
+import Navbar from "./components/Navbar/Navbar.js";
 import Footer from "./components/Footer/Footer.js";
 import Home from "./containers/Home/Home.js";
 
 function App() {
   return (
     <>
-      <Navabar />
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,8 +1,28 @@
 import React from 'react'
+import logo from '../../assets/logo/logo.jpg'
+import { IoMdPerson } from "react-icons/io";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='p-0'>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light h-20 flex justify-between items-center px-4">
+        <div className="navbar-name flex h-10">
+          <div className="navbar-logo w-16 h-16 justify-center items-center">
+            <img src={logo} alt="Logo" />
+          </div>
+          <a className="navbar-brand my-2" href="/">StudyNest</a>
+        </div>
+        <ul className="flex justify-center gap-4">
+          <li><a href="/Property">Property</a></li>
+          <li><a href="/Services">Services</a></li>
+          <li className='text-2xl'>
+            <button>
+              <IoMdPerson />
+            </button>
+          </li>
+        </ul>
+      </nav>
+    </div>
   )
 }
 
